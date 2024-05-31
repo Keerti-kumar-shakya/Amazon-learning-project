0eb818fac1a34074f5432be2269413a2e926008c
Keerti-kumar-shakya <keerti.shakya0008@gmail.com>
2024-05-31 20:04:54 +0530

    make delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -79,3 +79,18 @@ export function calculateCartQuantity() {
 
   return cartQuantity;
 }
+
+
+export function updateDeliveryOptions(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach( (cartItem) =>{
+   if (productId === cartItem.productId) {
+     matchingItem = cartItem;
+   }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity} from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOptions} from "../data/cart.js";
 import {products} from '../data/products.js';
 import { formatCurrency } from "./utils/money.js";
 //import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -100,7 +100,11 @@ cart.forEach((cartItem) => {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
  
     html += `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option"
+    data-product-id = "${matchingProduct.id}"
+    data-delivery-option-id = "${deliveryOption.id}"
+    >
+   
             <input type="radio"
             ${isChecked ? 'checked' : ''}
               class="delivery-option-input"
@@ -135,6 +139,13 @@ cart.forEach((cartItem) => {
      updateCartQuantity()
    });
    })
+
+   document.querySelectorAll('.js-delivery-option').forEach((element) =>{
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOptions(productId, deliveryOptionId);
+    });
+   });
 
 
    document.querySelectorAll('.js-update-link')

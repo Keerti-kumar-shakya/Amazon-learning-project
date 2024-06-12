6199623279b44aa40dfeae16f8d1d9560f494f19
Keerti-kumar-shakya <keerti.shakya0008@gmail.com>
2024-06-12 13:31:41 +0530

    Create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,111 @@
+class Cart {
+  cartItems;
+  #localStorageKey;
+
+  constructor(localStorageKey) {
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
+  }
+
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
+
+    if (!this.cartItems) {
+      this.cartItems = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+      }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+      }];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: '1'
+      });
+    }
+
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    const newCart = [];
+
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+
+    this.cartItems = newCart;
+
+    this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    this.saveToStorage();
+  }
+}
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart);
+
+
+//IMPORTANT: class generally = object generator
+
+//IMPORTANT: Object-Oriented Programming = organizing our code into objects (tries to represent the real world)
+
+//Class
+// = help us generate these objects
+
+//benefits of class oops
+
+//1.) A class looks like the object the it generates and cleaner then function based oops.
+
+//2.) Classes have extra features for Object-Oriented Programming
+
+// class based feature
+//1.) lets us run  setup code.
+
+//IMPORTANT: More details about constructor:
+//1.) Has to be named "constructor"
+//2.) Should not return anything from constructor.
+
+
+
+

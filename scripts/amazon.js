eff7c221d27674561357e6bd7941e44845194c8f
Keerti-kumar-shakya <keerti.shakya0008@gmail.com>
2024-06-12 14:57:34 +0530

    Convert products  into classes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -21,14 +21,14 @@ products.forEach(function(product) {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
+              src="${product.getStarsUrl()}">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${formatCurrency(product.priceCents)}
+          ${product.getPrice()}
           </div>
 
           <div class="product-quantity-container">
@@ -45,6 +45,8 @@ products.forEach(function(product) {
               <option value="10">10</option>
             </select>
           </div>
+
+          ${product.extraInfoHTML()}
 
           <div class="product-spacer"></div>
 
